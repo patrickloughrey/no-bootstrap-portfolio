535fd113cda30a0cd41c5051adcb2775efcb1ed0
Patrick Loughrey <PatrickLoughrey@nwk--192-217.nwk.ruw.rutgers.edu>
2017-03-10 14:12:13 -0500

    Added slick scroller autoplay

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,22 +1,112 @@
-/* Header.js Plug-In for On-Scroll Navbar */
+/* Slick.js Plug-In for Autoplay Scroller */
 
-$(".header").headroom({
-  "offset": 205,
-  "tolerance": 5,
-  "classes": {
-    "initial": "animated",
-    "pinned": "swingInX",
-    "unpinned": "swingOutX"
-  }
-});
+$(document).ready(function() {
 
-/* Slick.js Plug-In for Slick scroller */
-$(document).ready(function(){
+  console.log("This executes");
   $('.autoplay').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    });
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows:false
+
+  });
+
 });
 
+
+/* Google Maps Functionality */
+function initMap() {
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 40.5009336, lng: -74.4486826},
+          zoom: 12,
+          scrollwheel: false,
+          styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ]
+        });
+
+    var marker = new google.maps.Marker({
+        position: {lat: 40.5009336, lng: -74.4486826},
+        map: map
+    });
+
+}
